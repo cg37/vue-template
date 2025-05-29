48847afc4d0186bea5d659774e63f836637241eb
Craig <cg37@outlook.com>
2025-05-29 21:53:31 +0800

    build: eslint换行规则

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,10 @@ export default defineConfig([
         "error",
         {
           endOfLine: "auto",
-          trailingComma: "none"
+          trailingComma: "none",
+          bracketSpacing: true, // 不在括号内加空格
+          arrayBracketNewlines: true, // 禁止数组括号换行
+          objectCurlyNewlines: "never" // 强制对象括号不换行
         }
       ],
       "vue/html-self-closing": [
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,12 @@ import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
+    path: "#",
+    redirect: "Home"
+  },
+  {
     path: "/home",
+    name: "Home",
     component: () => import("../pages/HomePage.vue")
   },
   {
